68df0f674d63c6f1966d3ff6338cdbf8bbabf562
Chris Yip <chris.yip87@gmail.com>
2018-10-30 16:45:35 -0700

    Having a working canvas with circles bouncing. Circle class defined

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     entry: "./lib/clickmania.js",
     output: {
-        filename: "./lib/bundle.js",
+        filename: "../lib/bundle.js",
     },
     devtool: 'source-map'
 };
